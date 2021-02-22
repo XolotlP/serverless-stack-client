835427fd27434028a0dd8e2b9c6199f79637326c
XolotlP <xolotl.mp@gmail.com>
2021-02-22 09:47:57 -0600

    Create login page

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ Amplify.configure({
     region: config.cognito.REGION,
     userPoolId: config.cognito.USER_POOL_ID,
     identityPoolId: config.cognito.IDENTITY_POOL_ID,
-    userPoolWebClient: config.cognito.APP_CLIENT_ID
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
   },
   Storage: {
     region: config.s3.REGION,
@@ -22,8 +22,9 @@ Amplify.configure({
     identityPoolId: config.cognito.IDENTITY_POOL_ID
   },
   API: {
-    endpointss: [
+    endpoints: [
       {
+        // we are telling amplify that we want to name our API
         name: "notes",
         endpoint: config.apiGateway.URL,
         region: config.apiGateway.REGION
